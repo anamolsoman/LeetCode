a4dcff377ccd261fbb50f855dd18ba7b989f5349
Anamol Soman <anamolsoman2@gmail.com>
2024-09-10 14:12:53 +0530

    Time: 77 ms (50.66%), Space: 54.6 MB (33.01%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+ let left = 0
+    let right = left + 1
+   while(right <= nums.length - 1){
+    if(nums[left] !== 0){
+        left++
+        right++
+    }
+    else{
+        if(nums[right] !== 0){
+            let temp = nums[left]
+            nums[left] = nums[right]
+            nums[right] = temp
+            left++
+        }
+        right ++
+      
+    } 
+}
+return nums
+};
