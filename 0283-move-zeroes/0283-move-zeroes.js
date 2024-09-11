e537e3a324994b219a1e785c8e2359f2fb020d47
Anamol Soman <anamolsoman2@gmail.com>
2024-09-11 07:28:45 +0530

    Time: 85 ms (22.7%), Space: 53.6 MB (85.67%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.js
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -3,18 +3,18 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
-    let pointerA = 0;
-    let pointerB = 0;
-
-    while (pointerB < nums.length) {
-        if (nums[pointerB] !== 0) {
-            let temp = nums[pointerA];
-            nums[pointerA] = nums[pointerB];
-            nums[pointerB] = temp;
-            pointerA++;
+    let left = 0
+    let lastNonZeroFound = 0
+    while(left < nums.length){
+        if(nums[left] != 0){
+            let temp 
+            temp = nums[left]
+            nums[left] = nums[lastNonZeroFound]
+            nums[lastNonZeroFound] = temp
+            lastNonZeroFound++
         }
-        pointerB++;
+            left++
+        
     }
- 
- return nums;
+    return nums
 };
