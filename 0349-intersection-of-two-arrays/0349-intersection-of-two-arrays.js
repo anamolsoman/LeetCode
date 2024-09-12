9256423aa749d97e5df4a5bf324da7328555a971
Anamol Soman <anamolsoman2@gmail.com>
2024-09-12 12:16:13 +0530

    Time: 77 ms (5.22%), Space: 52.3 MB (5.38%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
@@ -3,15 +3,7 @@
  * @param {number[]} nums2
  * @return {number[]}
  */
-
-// Optimised way 
-
-// var intersection = function(nums1, nums2) {
-//      if (nums1.length > nums2.length) return intersection(nums2, nums1)
-// 	let set = new Set(nums1), ans = new Set()
-// 	for (let num of nums2) if (set.has(num)) ans.add(num);
-// 	return [...ans]
-// };
+ 
 
 var intersection = function(nums1, nums2) {
     if(nums1.length > nums2.length) intersection(nums2,nums1)
