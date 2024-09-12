22ec2f75ac58d9b81ebce9cd1f462649524b3dcc
Anamol Soman <anamolsoman2@gmail.com>
2024-09-12 12:18:33 +0530

    Time: 59 ms (36%), Space: 50.6 MB (47.97%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
@@ -5,29 +5,8 @@
  */
 
 var intersection = function(nums1, nums2) {
-    let smallArr = nums1.length <= nums2.length ? nums1 : nums2
-    let largeArr = nums1.length > nums2.length ? nums1 : nums2
-    console.log(smallArr,largeArr)
-    let pointer = 0
-    let pointer2 = 0
-    let intersection = []
-
-    while(pointer < smallArr.length){
-        if(smallArr[pointer] === largeArr[pointer2] && !intersection.includes(smallArr[pointer])){
-            intersection.push(smallArr[pointer])
-            pointer++
-            pointer2 = 0
-        }else{
-            if(pointer2 < largeArr.length){
-                       pointer2++
-            }else{
-                pointer++
-                pointer2=0
-            }
-       
-        }
-    }
-
-
-    return intersection
+    if (nums1.length > nums2.length) return intersection(nums2, nums1)
+	let set = new Set(nums1), ans = new Set()
+	for (let num of nums2) if (set.has(num)) ans.add(num);
+	return [...ans]
 };
