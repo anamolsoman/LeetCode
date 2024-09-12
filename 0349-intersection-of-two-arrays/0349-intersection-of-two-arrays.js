bef00f4807e776d38ab8161b539c49cd91c9943f
Anamol Soman <anamolsoman2@gmail.com>
2024-09-12 12:16:51 +0530

    Time: 76 ms (5.22%), Space: 50.9 MB (36.16%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
@@ -3,22 +3,22 @@
  * @param {number[]} nums2
  * @return {number[]}
  */
- 
 
 var intersection = function(nums1, nums2) {
-    if(nums1.length > nums2.length) intersection(nums2,nums1)
-    console.log(nums1,nums2)
+    let smallArr = nums1.length <= nums2.length ? nums1 : nums2
+    let largeArr = nums1.length > nums2.length ? nums1 : nums2
+    console.log(smallArr,largeArr)
     let pointer = 0
     let pointer2 = 0
-    let ans = []
+    let intersection = []
 
-    while(pointer < nums1.length){
-        if(nums1[pointer] === nums2[pointer2] && !ans.includes(nums1[pointer])){
-            ans.push(nums1[pointer])
+    while(pointer < smallArr.length){
+        if(smallArr[pointer] === largeArr[pointer2] && !intersection.includes(smallArr[pointer])){
+            intersection.push(smallArr[pointer])
             pointer++
             pointer2 = 0
         }else{
-            if(pointer2 < nums2.length){
+            if(pointer2 < largeArr.length){
                        pointer2++
             }else{
                 pointer++
@@ -29,5 +29,5 @@ var intersection = function(nums1, nums2) {
     }
 
 
-    return ans
+    return intersection
 };
